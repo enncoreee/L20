0c17bb6196a0f81625056b15b79322da12ea1834
En <24013373@myrp.edu.sg>
2025-07-17 12:16:27 +0800

    db connection

--- app.js
+++ app.js
@@ -10,10 +10,14 @@ const app = express();
 
 // Database connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'C237_usersdb'
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'C237_usersdb'
+    host: 'c237-all.mysql.database.azure.com',
+    user: 'c237admin',
+    password: 'c2372025!',
+    database: 'c237_24013373'
 });
 
 db.connect((err) => {
